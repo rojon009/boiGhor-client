fe9eb3f55cc71ddff9f187d752d25a3bd3566155
rojon <rjrojon@gmail.com>
2021-04-02 20:14:42 +0600

    readme file added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,27 +4,42 @@ import BookCard from '../../components/BookCard/BookCard';
 
 export
 
-const Home = () => {
+    const Home = () => {
 
-    const [books, setBooks] = useState(null);
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_SERVER}/books`)
-        .then(res => res.data)
-        .then(data => setBooks(data))
-    }, [])
+        const [loading, setLoading] = useState(true);
 
-    if(books) {
-        return (
-            <div className="container pt-5">
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4">
+        const [books, setBooks] = useState(null);
+        useEffect(() => {
+            axios.get(`${process.env.REACT_APP_SERVER}/books`)
+                .then(res => res.data)
+                .then(data => {setBooks(data); setLoading(false)})
+        }, [])
+
+        if (books) {
+            return (
+                <>
+                    
+                    <div className="container pt-5">
+                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4">
+                            {
+                                books.map(book => <BookCard key={book._id} {...book} />)
+                            }
+                        </div>
+                    </div>
+                </>
+            );
+        }
+        return <>
                     {
-                        books.map(book => <BookCard key={book._id} {...book} />)
+                        loading && (
+                            <div class="d-flex justify-content-center mt-5 pt-5">
+                                <div class="spinner-border" role="status">
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
+                            </div>
+                        )
                     }
-                </div>
-            </div>
-        );
-    }
-    return <></>;
-};
+            </>;
+    };
 
 export default Home;
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -8,6 +8,7 @@ const Orders = () => {
     const [loggedInUser] = useContext(UserContext);
     const {uid} = useParams();
     const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         if(loggedInUser?.uid) {
             let orderedBooks = []
@@ -30,38 +31,51 @@ const Orders = () => {
             <h4 className="text-warning text-center mt-5">You haven't order any book yet</h4>
         )
     }
+    if(books) {
 
-    return(
-        <div className="container-fluid p-5">
-            <table className="table border">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">NAME</th>
-                        <th scope="col">AUTHOR</th>
-                        <th scope="col">PRICE</th>
-                        <th scope="col">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        books?.map((book, index) => (
-                            <tr key={book?._id} >
-                                <th scope="row">{index + 1}</th>
-                                <td>{book?.name}</td>
-                                <td>{book?.author}</td>
-                                <td>{book?.price}</td>
-                                <td>
-                                    <small className="text-success">Order is taken</small>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-        </div>
-
-    );
+        return(
+            <div className="container-fluid p-5">
+                <table className="table border">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NAME</th>
+                            <th scope="col">AUTHOR</th>
+                            <th scope="col">PRICE</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            books?.map((book, index) => (
+                                <tr key={book?._id} >
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{book?.name}</td>
+                                    <td>{book?.author}</td>
+                                    <td>{book?.price}</td>
+                                    <td>
+                                        <small className="text-success">Order is taken</small>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+    
+        );
+    }
+    return (
+        <>
+            {loading && (
+                <div class="d-flex justify-content-center">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            )}
+        </>
+    )
 };
 
 export default Orders;
