fe0e4e321c527ff631d738a59cb864c43a86c0ea
rojon <rjrojon@gmail.com>
2021-04-02 23:24:26 +0600

    firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,4 @@
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "fullstack-bookshop.firebaseapp.com",
@@ -6,4 +7,6 @@ const firebaseConfig = {
   messagingSenderId: "336413137220",
   appId: "1:336413137220:web:980d59d186edccf2445f11"
 };
+
+
 export default firebaseConfig;
